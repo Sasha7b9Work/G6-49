97d5603634e25ca72f9d3c972192806a093fddcd
Sasha7b9 <Sasha7b9@tut.by>
2018-02-16 16:50:40 +0300

    Рефакторинг

--- sources/Panel/src/Display/Painter.h
+++ sources/Panel/src/Display/Painter.h
@@ -31,14 +31,8 @@ public:
     static void RunDisplay();
     /// Вызывается в конце отрисовки каждого кадра. Переносит содержимое буфера на экран
     static void EndScene();
-    /// Послать изображение во внешнее устройство через USB или LAN. Если first == true, то посылается шрифт
-    static void SendFrame(bool first);
-    /// Сброс таймера мигания. Нужно для того, чтобы мигающие значки при перемещении не исчезали с экрана
-    static void ResetFlash();
     /// Установить цвет рисования
     static void SetColor(Color color);
-
-    static void LoadPalette();
 
     static void SetPoint(int x, int y);
 
@@ -46,36 +40,11 @@ public:
 
     static void DrawVLine(int x, int y0, int y1);
 
-    static void DrawHPointLine(int y, int x0, int x1, float delta);
-
-    static void DrawVPointLine(int x, int y0, int y1, float delta);
-
-    static void DrawMultiVPointLine(int numLines, int y, uint16 x[], int delta, int count, Color color);
-
-    static void DrawMultiHPointLine(int numLines, int x, uint8 y[], int delta, int count, Color color);
-
     static void DrawLine(int x0, int y0, int x1, int y1);
-    /// \brief Рисует прерывистую горизонтальную линию. dFill - длина штриха, dEmpty - расст. между штрихами. Линия всегда начинается со штриха. 
-    /// dStart указывает смещение первой рисуемой точки относительно начала штриха.
-    static void DrawDashedHLine(int y, int x0, int x1, int dFill, int dEmpty, int dStart);
-    /// Рисует прерывистую вертикальную линию.
-    static void DrawDashedVLine(int x, int y0, int y1, int dFill, int dEmpty, int dStart);
 
     static void DrawRectangle(int x, int y, int width, int height);
 
     static void FillRegion(int x, int y, int width, int height);
-
-    static void DrawVolumeButton(int x, int y, int width, int height, int thickness, Color normal, Color bright, Color dark, bool isPressed, bool isShade);
-    /// Установить яркость дисплея.
-    static void SetBrightnessDisplay(int16 brightness);
-
-    static uint16 ReduceBrightness(uint16 colorValue, float newBrightness);
-    /// Нарисовать массив вертикальных линий. Линии рисуются одна за другой. y0y1 - массив вертикальных координат.
-    static void DrawVLineArray(int x, int numLines, uint8 *y0y1, Color color);
-    /// modeLines - true - точками, false - точками.
-    static void DrawSignal(int x, uint8 data[281], bool modeLines);
-
-    static void DrawPicture(int x, int y, int width, int height, uint8 *address);
 
     static void DrawBigText(int x, int y, int size, const char *text);
 
--- sources/Panel/src/Display/PainterC.h
+++ sources/Panel/src/Display/PainterC.h
@@ -18,41 +18,5 @@ public:
     static void DrawRectangle(int x, int y, int width, int height, Color color);
 };
 
-#define Painter_SetPointC(x, y, color)                                      \
-    Painter::SetColor(color);                                                \
-    Painter::SetPoint(x, y);
-
-#define Painter_DrawVLineC(x, y0, y1, color)                                \
-    Painter::SetColor(color);                                                \
-    Painter::DrawVLine(x, y0, y1);
-
-#define Painter_DrawHLineC(y, x0, x1, color)                                \
-    Painter::SetColor(color);                                                \
-    Painter::DrawHLine(y, x0, x1);
-
-#define Painter_DrawLineC(x0, y0, x1, y1, color)                            \
-    Painter::SetColor(color);                                                \
-    Painter::DrawLine(x0, y0, x1, y1);
-
-#define Painter_DrawDashedHLineC(y, x0, x1, dFill, dEmpty, dStart, color);  \
-    Painter::SetColor(color);                                                \
-    Painter::DrawDashedHLine(y, x0, x1, dFill, dEmpty, dStart);
-
-#define Painter_DrawDashedVLineC(x, y0, y1, dFill, dEmpty, dStart, color);  \
-    Painter::SetColor(color);                                                \
-    Painter::DrawDashedVLine(x, y0, y1, dFill, dEmpty, dStart);
-
-#define Painter_Draw4SymbolsInRectC(x, y, eChar, color)                     \
-    Painter::SetColor(color);                                                \
-    Painter::Draw4SymbolsInRect(x, y, eChar);
-
-#define Painter_DrawTextInRectC(x, y, width, text, color)                   \
-    Painter::SetColor(color);                                                \
-    Painter::DrawTextInRect(x, y, width, text);
-
-#define Painter_DrawTextRelativelyRightC(x, y, text, color)                 \
-    Painter::SetColor(color);                                                \
-    Painter::DrawTextRelativelyRight(x, y, text);
-
 /** @}  @}
  */
