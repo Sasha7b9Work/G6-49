9d3e2f6fe2c79a7a6327b4c43251eed194077639
Sasha7b9 <Sasha7b9@tut.by>
2018-05-25 17:15:27 +0300

    Panel Неудачная попытка добавать новый параметр к синус

--- sources/Panel/src/globals.cpp
+++ sources/Panel/src/globals.cpp
@@ -6,20 +6,32 @@ InputWindowStruct sig_structParameter[NumChannels][NumForms][NumParameters] =
 {
             {
             {                                                                           // [A][Form_Sine]
-                {"20000", "", 0, 0, NUM_DIGITS - 1, Kilo,  Frequency, Sign_None, 1, 0},
-                {"10000", "", 0, 0, NUM_DIGITS - 1, Milli, Period,    Sign_None, 1, 0},
-                {"50000", "", 0, 0, NUM_DIGITS - 1, One,   Amplitude, Sign_None, 1, 0},
-                {"00000", "", 0, 0, NUM_DIGITS - 1, One,   Offset,    Sign_Plus, 1, 0},
-                {},
-                {},
-                {},
-                {}
+/* Frequency */ {"20000", "", 0, 0, NUM_DIGITS - 1, Kilo,  Frequency, Sign_None, 1, 0},
+/* Period    */ {"10000", "", 0, 0, NUM_DIGITS - 1, Milli, Period,    Sign_None, 1, 0},
+/* Amplitude */ {"50000", "", 0, 0, NUM_DIGITS - 1, One,   Amplitude, Sign_None, 1, 0},
+/* Offset    */ {"00000", "", 0, 0, NUM_DIGITS - 1, One,   Offset,    Sign_Plus, 1, 0},
+/* Duration  */ {},
+/* DutyRatio */ {},
+/* Phase     */ {},
+/* Delay     */ {},
+/* DepthMod  */ {"00000", "", 0, 0, NUM_DIGITS - 1, One,  DepthModulation, Sign_None, 1, 0},
+/* Polarity  */ {},
+/* DurRise   */ {},
+/* DurFall   */ {},
+/* DurStady  */ {},
+/* DurFactor */ {}
             },
             {                                                                           // [A][Form_Saw]
                 {"10000", "", 0, 0, NUM_DIGITS - 1, Kilo,  Frequency, Sign_None, 1, 0},
                 {"10000", "", 0, 0, NUM_DIGITS - 1, Milli, Period,    Sign_None, 1, 0},
                 {"50000", "", 0, 0, NUM_DIGITS - 1, One,   Amplitude, Sign_None, 1, 0},
                 {"00000", "", 0, 0, NUM_DIGITS - 1, One,   Offset,    Sign_Plus, 1, 0},
+                {},
+                {},
+                {},
+                {},
+                {},
+                {},
                 {},
                 {},
                 {},
@@ -32,6 +44,12 @@ InputWindowStruct sig_structParameter[NumChannels][NumForms][NumParameters] =
                 {"00000", "", 0, 0, NUM_DIGITS - 1, One,   Offset,    Sign_Plus, 1, 0},
                 {"10000", "", 2, 0, NUM_DIGITS - 1, Micro, Duration,  Sign_None, 1, 0},
                 {"20000", "", 0, 0, NUM_DIGITS - 1, One,   DutyRatio, Sign_None, 1, 0},
+                {},
+                {},
+                {},
+                {},
+                {},
+                {},
                 {},
                 {}
             }
