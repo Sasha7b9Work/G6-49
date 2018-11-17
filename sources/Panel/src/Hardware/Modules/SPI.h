2b18ca278c5af902ef8ff16e45b9212845e5e553
Sasha7b9 <sepultura_08>
2018-11-18 02:41:26 +0300

    *

--- sources/Panel/src/Hardware/Modules/SPI.h
+++ sources/Panel/src/Hardware/Modules/SPI.h
@@ -3,16 +3,16 @@
 
 class SPI4_
 {
-	friend class CPU;
-	private:
-		static void Init();
-	public:
-		/// Переслать массив данных
-		static bool Transmit(const void *buffer, uint size);
-		/// Принять массив данных
-		static bool Receive(void *recv, uint size);
-		/// Возвращает true, если прибор готов к обмену
-		static bool IsReady();
-		/// Ждать пока освободится устройство
-		static void WaitFreedom();
+friend class CPU;
+private:
+    static void Init();
+public:
+    /// Переслать массив данных
+    static bool Transmit(const void *buffer, uint size);
+    /// Принять массив данных
+    static bool Receive(void *recv, uint size);
+    /// Возвращает true, если прибор готов к обмену
+    static bool IsReady();
+    /// Ждать пока освободится устройство
+    static void WaitFreedom();
 };
