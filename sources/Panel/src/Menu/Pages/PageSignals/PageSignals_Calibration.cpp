c3db94b0981a50744ddb557363bb0607f25443ab
Sasha7b9 <Sasha7b9@tut.by>
2018-11-20 09:02:24 +0300

    Device Страница калибровки AD9952. Есть интерфейс, но пока не работает

--- sources/Panel/src/Menu/Pages/PageSignals/PageSignals_Calibration.cpp
+++ sources/Panel/src/Menu/Pages/PageSignals/PageSignals_Calibration.cpp
@@ -2,6 +2,7 @@
 #ifndef WIN32
 #include "Menu/MenuItems.h"
 #include "Menu/Pages/Include/PageSignals.h"
+#include "Settings/Settings.h"
 #endif
 
 
@@ -11,12 +12,69 @@ Page *PageSignals::PageCalibration::pointer = (Page *)&pageCalibration;
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-DEF_PAGE_4( pageCalibration,
+static void OnChange_NegativeAD9952()
+{
+
+}
+
+DEF_GOVERNOR( gNegativeAD9952,                                                                                                         //--- НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - AD9952 -5В ---
+    "AD9952 -5В", "AD9952 -5V",
+    "Настройка уровня -5В AD9952",
+    "Level setting -5V AD9952",
+    CAL_AD9952_OFFSET_NEG, -128, 128, pageCalibration, FuncActive, OnChange_NegativeAD9952, EmptyFuncVV
+)
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+static void OnChange_PositiveAD9952()
+{
+
+}
+
+DEF_GOVERNOR( gPositiveAD9952,                                                                                                         //--- НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - AD9952 +5В ---
+    "AD9952 +5В", "AD9952 +5V",
+    "Настройка уровня +5В AD9952",
+    "Level settings +5V AD9952",
+    CAL_AD9952_OFFSET_POS, -128, 128, pageCalibration, FuncActive, OnChange_PositiveAD9952, EmptyFuncVV
+)
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+static void OnChange_ZeroAD9952()
+{
+
+}
+
+DEF_GOVERNOR( gZeroAD9952,                                                                                                              //--- НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - AD9952 0В ---
+    "AD9952 0В", "AD9952 0V",
+    "Настройка уровня +5В AD9952",
+    "Level settings +5V AD9952",
+    CAL_AD9952_OFFSET_ZERO, -128, 128, pageCalibration, FuncActive, OnChange_ZeroAD9952, EmptyFuncVV
+)
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+static void OnChange_AmplitudeAD9952()
+{
+
+}
+
+DEF_GOVERNOR( gAmplitudeAD9952,                                                                                                     //--- НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - AD9952 Размах ---
+    "AD9952 Размах", "AD9952 Amplitude",
+    "Настройка размаха AD9952",
+    "Amplitude settings AD9952",
+    CAL_AD9952_AMPLITUDE, -128, 128, pageCalibration, FuncActive, OnChange_AmplitudeAD9952, EmptyFuncVV
+)
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+DEF_PAGE_4( pageCalibration,                                                                                                                        //--- НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА ---
     "КАЛИБРОВКА", "CALIBRATION",
     "", "",
-    0,      ///< НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - Синус +5В
-    0,      ///< НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - Синус 0В
-    0,      ///< НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - Синус -5В
-    0,      ///< НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - Синус размах
+    &gPositiveAD9952,       ///< НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - AD9952 +5В
+    &gZeroAD9952,           ///< НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - AD9952 0В
+    &gNegativeAD9952,       ///< НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - AD9952 -5В
+    &gAmplitudeAD9952,      ///< НАСТРОЙКИ СИГНАЛОВ - КАЛИБРОВКА - AD9952 размах
     Page::Settings_Calibration, PageSignals::pointer, FuncActive, FuncPress, FuncOnKey, FuncDrawPage
 )
