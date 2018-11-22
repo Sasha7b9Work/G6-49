98e843c8a2e9dd4dcd6633b1793c65db288f3306
Sasha7b9 <Sasha7b9@tut.by>
2018-11-22 10:39:16 +0300

    Структура калибровочных настроек модифицирована под одинаковый размер

--- sources/Device/src/Settings/CalibrationSettings.h
+++ sources/Device/src/Settings/CalibrationSettings.h
@@ -13,15 +13,28 @@
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef WIN32
+#pragma anon_unions
+#endif
+
 struct CalibrationSettings
 {
-    const   uint marker;                                ///< По этому значению загрузчик настроек определяет, что они есть
-    int16   cal_AD9952_Offset_Negative[Chan::Number];   ///< Калибровочный коэффициент AD9952 на -5В
-    int16   cal_AD9952_Offset_Zero[Chan::Number];       ///< Калибровочный коэффициент AD9952 на 0В
-    int16   cal_AD9952_Offset_Positive[Chan::Number];   ///< Калибровочный коэффициент AD9952 на +5В
-    int16   cal_AD9952_Amplitude[Chan::Number];         ///< Калибровочный коэффициент AD9952 размаха
-    int16   cal_DDS_MAX[Chan::Number];
-    int16   cal_DDS_MIN[Chan::Number];
+    union
+    {
+        struct
+        {
+            const   uint marker;                                ///< По этому значению загрузчик настроек определяет, что они есть
+            int16   cal_AD9952_Offset_Negative[Chan::Number];   ///< Калибровочный коэффициент AD9952 на -5В
+            int16   cal_AD9952_Offset_Zero[Chan::Number];       ///< Калибровочный коэффициент AD9952 на 0В
+            int16   cal_AD9952_Offset_Positive[Chan::Number];   ///< Калибровочный коэффициент AD9952 на +5В
+            int16   cal_AD9952_Amplitude[Chan::Number];         ///< Калибровочный коэффициент AD9952 размаха
+            int16   cal_DDS_MAX[Chan::Number];
+            int16   cal_DDS_MIN[Chan::Number];
+        };
+        uint8 unused[512];
+    };
+    
 };
 
 
