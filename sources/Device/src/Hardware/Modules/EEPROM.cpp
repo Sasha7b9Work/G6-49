38481ad14d4f605fabb996cf6e54300ade0a75e4
Sasha7b9 <Sasha7b9@tut.by>
2018-11-22 11:40:54 +0300

    Device Сделано сохранение настроек. Не проверялось.

--- sources/Device/src/Hardware/Modules/EEPROM.cpp
+++ sources/Device/src/Hardware/Modules/EEPROM.cpp
@@ -6,20 +6,20 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-#define ADDR_SECTOR_0   ((uint *)0x08000000)  // 16k  Загрузчик
-#define ADDR_SECTOR_1   ((uint *)0x08004000)  // 16k
-#define ADDR_SECTOR_2   ((uint *)0x08008000)  // 16k
-#define ADDR_SECTOR_3   ((uint *)0x0800c000)  // 16k
-#define ADDR_SECTOR_4   ((uint *)0x08010000)  // 64k  SettingsCalibration
+#define ADDR_SECTOR_0   ((uint)0x08000000)  // 16k  Загрузчик
+#define ADDR_SECTOR_1   ((uint)0x08004000)  // 16k
+#define ADDR_SECTOR_2   ((uint)0x08008000)  // 16k
+#define ADDR_SECTOR_3   ((uint)0x0800c000)  // 16k
+#define ADDR_SECTOR_4   ((uint)0x08010000)  // 64k  SettingsCalibration
 #define ADDR_SECTOR_CALIBRATION ADDR_SECTOR_4
 #define SIZE_SECTOR_CALIBRATION (64 * 1024)
-#define ADDR_SECTOR_5   ((uint *)0x08020000)  // 128k Основная прошивка 1
-#define ADDR_SECTOR_6   ((uint *)0x08040000)  // 128k Основная прошивка 2
-#define ADDR_SECTOR_7   ((uint *)0x08060000)  // 128k Основная прошивка 3
-#define ADDR_SECTOR_8   ((uint *)0x08080000)  // 128k
-#define ADDR_SECTOR_9   ((uint *)0x080a0000)  // 128k
-#define ADDR_SECTOR_10  ((uint *)0x080c0000)  // 128k
-#define ADDR_SECTOR_11  ((uint *)0x080e0000)  // 128k
+#define ADDR_SECTOR_5   ((uint)0x08020000)  // 128k Основная прошивка 1
+#define ADDR_SECTOR_6   ((uint)0x08040000)  // 128k Основная прошивка 2
+#define ADDR_SECTOR_7   ((uint)0x08060000)  // 128k Основная прошивка 3
+#define ADDR_SECTOR_8   ((uint)0x08080000)  // 128k
+#define ADDR_SECTOR_9   ((uint)0x080a0000)  // 128k
+#define ADDR_SECTOR_10  ((uint)0x080c0000)  // 128k
+#define ADDR_SECTOR_11  ((uint)0x080e0000)  // 128k
 
 #define CLEAR_FLASH_FLAGS \
     __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP    |  /* end of operation flag              */   \
@@ -32,20 +32,22 @@
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// Возвращает первый адрес, значение в котором не равно 0xffffffff. Поиск начинается с адреса start, продолжается в участке памяти размером sizeFull. Кратно размеру sizeObject
-static uint *FirstNotFilledAddress(uint *start, uint sizeFull, uint sizeObject);
+/// Возвращает первый адрес, значение в котором равно 0xffffffff (можно записывать). Поиск начинается с адреса start, продолжается в участке памяти размером sizeFull. Кратно размеру sizeObject
+static uint FindFirstFreeRecord(uint start, uint sizeSector, uint sizeRecord);
+/// Возвращает адрес последнего блока, в котором первый байт не равен 0xffffffff (в нём сохраенены последние настройки)
+static uint FindLastOccupiedRecord(uint start, uint sizeSector, uint sizeRecord);
 /// Стирает сектор с начальным адресом startAddress
-static void EraseSector(uint *startAddress);
+static void EraseSector(uint startAddress);
 /// Записывает size байт из массива data по адресу address
-static void WriteData(uint *address, void *data, uint size);
+static void WriteData(uint address, void *data, uint size);
 /// Возвращает системный идентификатор сектора с начальным адресом address. Ежели такового нету, возвращает -1
-static uint GetSector(uint *address);
+static uint GetSector(uint address);
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void EEPROM::SaveSettings(CalibrationSettings *settings)
 {
-    uint *address = FirstNotFilledAddress(ADDR_SECTOR_CALIBRATION, SIZE_SECTOR_CALIBRATION, sizeof(CalibrationSettings));
+    uint address = FindFirstFreeRecord(ADDR_SECTOR_CALIBRATION, SIZE_SECTOR_CALIBRATION, sizeof(CalibrationSettings));
 
     if (address == 0)
     {
@@ -57,31 +59,54 @@ void EEPROM::SaveSettings(CalibrationSettings *settings)
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-bool EEPROM::LoadSettings(CalibrationSettings * /*settings*/)
+void EEPROM::LoadSettings(CalibrationSettings *settings)
 {
-    return false;
+    uint address = FindLastOccupiedRecord(ADDR_SECTOR_CALIBRATION, SIZE_SECTOR_CALIBRATION, sizeof(CalibrationSettings));
+
+    if (address)                                            // Если нашли сохранённую запись
+    {
+        *settings = *((CalibrationSettings *)address);      // То запишем её в целевой объект
+    }
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-static uint *FirstNotFilledAddress(uint *start, uint sizeFull, uint sizeObject)
+static uint FindFirstFreeRecord(uint start, uint sizeFull, uint sizeRecord)
 {
-    uint *address = start;
-    uint *end = start + sizeFull;
+    uint address = start;
+    uint end = start + sizeFull;
 
     while (address < end)
     {
-        if (*address != 0xffffffff)
+        if (*(uint *)address == 0xffffffff)
         {
             return address;
         }
-        address += sizeObject;
+        address += sizeRecord;
     }
 
     return 0;
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-static void EraseSector(uint *startAddress)
+static uint FindLastOccupiedRecord(uint start, uint sizeSector, uint sizeRecord)
+{
+    uint address = FindFirstFreeRecord(start, sizeSector, sizeRecord);
+
+    if (address == 0)                               // Если свободной записи нет, значит, весь сектора заполнен
+    {
+        return start + sizeSector - sizeRecord;     // надо считвать последнюю запись
+    }
+
+    if (address == start)                           // Если первая свободная запись находится в начале сектора, то сектор пуст - запись в него не производилась
+    {
+        return 0;                                   // Возвращаем 0 как признак того, что записей нет
+    }
+
+    return address - sizeRecord;                    // Во всех остальных случаях возвращаем адрес записи, предыдущей по отношению к первой свободной
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+static void EraseSector(uint startAddress)
 {
     if (GetSector(startAddress) == (uint)-1)
     {
@@ -106,12 +131,12 @@ static void EraseSector(uint *startAddress)
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-static uint GetSector(uint *address)
+static uint GetSector(uint address)
 {
     struct StructSector
     {
         uint number;
-        uint *startAddress;
+        uint startAddress;
     };
 
     static const StructSector sectors[] =
@@ -134,13 +159,11 @@ static uint GetSector(uint *address)
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-static void WriteData(uint *pointer, void *data, uint size)
+static void WriteData(uint address, void *data, uint size)
 {
     CLEAR_FLASH_FLAGS;
 
     HAL_FLASH_Unlock();
-
-    uint address = (uint)pointer;
 
     for (uint i = 0; i < size; i++)
     {
