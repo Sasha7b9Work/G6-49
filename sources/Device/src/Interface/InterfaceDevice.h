9e6b4ed5636336b3d7e0800561f2555c4caccc54
Sasha7b9 <Sasha7b9@tut.by>
2018-11-23 14:14:08 +0300

    Удаление мусора

--- sources/Device/src/Interface/InterfaceDevice.h
+++ sources/Device/src/Interface/InterfaceDevice.h
@@ -11,36 +11,11 @@ class Interface
 
 public:
 
-    /// Структура для работы с интерфейсом
-    struct Data
-    {
-    public:
-
-        Data(uint size, uint8 command = 0);
-
-        ~Data();
-        /// Инициализировать структуры для хранения size байт данных
-        bool Init(uint size);
-        /// Освободить память, использованную структурой. Освобождать сразу же после использования
-        void Release();
-        /// Возвращает true, если буфер пуст - нулевой размер
-        bool IsEmpty() const;
-        /// Возвращает указатель на данные
-        uint8 *GetData();
-        /// Возвращает размер буфера данных
-        uint GetSize() const;
-    private:
-        uint8 *data;
-        uint size;
-    };
-
     static void Update();
     /// Добавляет сообщение в очередь отправки. Если очередь заполнена, сообщение удаляется и функция возвращает false
     static bool AddMessageInQueue(Message *message);
 
 private:
-
-    static uint timeLastReceive;
     /// Очередь сообщений, ожидающих отправки
     static Queue messages;
     /// Ненулевое значение означает, что его следует передать в панель как измеренное частотомером значение
