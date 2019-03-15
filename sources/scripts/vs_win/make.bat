6cd006badb711b2965bf0003a8c82c452a5ed759
Sasha7b9 <Sasha7b9@tut.by>
2019-03-15 09:11:49 +0300

    GUI-версия. Собирается из командной строки в виде lib

--- sources/scripts/vs_win/make.bat
+++ sources/scripts/vs_win/make.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 @echo .
 @echo %TIME%   Making Visual Studio Win Debug ...
@@ -9,6 +9,6 @@ rmdir "generated/Win32" /s /q
 
 cd scripts/vs_win
 
-cmake ../../VS/CMakeLists.txt -B../../generated/Win32 > cmake_out.txt
+cmake ../../VS/CMakeLists.txt -B../../generated/Win32
 
 @echo %TIME%   Complete
