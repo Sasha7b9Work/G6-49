223b43b2bf61f238c50a5afb6c5c592490c74f53
Sasha7b9 <Sasha7b9@tut.by>
2019-06-19 10:30:44 +0300

    Не работает после рефакторинга

--- sources/Panel/src/FDrive/FDrivePanel.cpp
+++ sources/Panel/src/FDrive/FDrivePanel.cpp
@@ -14,6 +14,21 @@
 #endif
 
 
+namespace FDrive
+{
+    namespace Handler
+    {
+        SimpleMessage *msg = nullptr;
+        bool E();
+        bool IsMount();
+        bool GetNumDirsAndFiles();
+        bool RequestFile();
+        bool RequestFileSize();
+        bool LoadFromExtStorage();
+    }
+}
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 enum Mount
 {
@@ -22,7 +37,7 @@ enum Mount
     Failed
 };
 
-static Mount mounted  = Disconnect;
+static Mount mounted = Disconnect;
 
 char FDrive::directory[255] = "\\";
 
@@ -76,40 +91,98 @@ void FDrive::Draw()
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-bool FDrive::Handler::Processing(SimpleMessage *msg)
+bool FDrive::Handler::Processing(SimpleMessage *message)
 {
+    msg = message;
+
     msg->ResetPointer();
 
-    Command::E command = (Command::E)msg->TakeByte();
+    Command::E com = (Command::E)msg->TakeByte();
 
-    if (command == Command::FDrive_Mount)
+    static const pFuncBV funcs[Command::Number] =
     {
-        mounted = (Mount)msg->TakeByte();
-        Init();
-        return true;
-    }
-    else if (command == Command::FDrive_NumDirsAndFiles)
-    {
-        return Items::Handler::Processing(msg);
-    }
-    else if (command == Command::FDrive_RequestDir)
-    {
-        return true;
-    }
-    else if (command == Command::FDrive_RequestFile)
-    {
-        return Items::Handler::Processing(msg);
-    }
-    else if (command == Command::FDrive_RequestFileSize)
-    {
-        return Items::Handler::Processing(msg);
-    }
-    else
-    {
-        // здесь ничего
-    }
+        /* RequestData                  */ EmptyFuncBV,
+        /* EnableChannel                */ EmptyFuncBV,
+        /* SetFormWave                  */ EmptyFuncBV,
+        /* SetFrequency                 */ EmptyFuncBV,
+        /* SetAmplitude                 */ EmptyFuncBV,
+        /* SetOffset                    */ EmptyFuncBV,
+        /* SetDuration                  */ EmptyFuncBV,
+        /* SetDutyRatio                 */ EmptyFuncBV,
+        /* SetPhase                     */ EmptyFuncBV,
+        /* RunReset                     */ EmptyFuncBV,
+        /* ModeDebug                    */ EmptyFuncBV,
+        /* SetDelay                     */ EmptyFuncBV,
+        /* WriteRegister                */ EmptyFuncBV,
+        /* SetDurationRise              */ EmptyFuncBV,
+        /* SetDurationFall              */ EmptyFuncBV,
+        /* SetDurationStady             */ EmptyFuncBV,
+        /* SetDutyFactor                */ EmptyFuncBV,
+        /* SetManipulation              */ EmptyFuncBV,
+        /* SetManipulationDuration      */ EmptyFuncBV,
+        /* SetManipulationPeriod        */ EmptyFuncBV,
+        /* SetPacketPeriod              */ EmptyFuncBV,
+        /* SetPacketNumber              */ EmptyFuncBV,
+        /* SetStartMode                 */ EmptyFuncBV,
+        /* SetPeriod                    */ EmptyFuncBV,
+        /* SetPolarity                  */ EmptyFuncBV,
+        /* SetManipulationMode          */ EmptyFuncBV,
+        /* LoadFromDDS                  */ EmptyFuncBV,
+        /* FreqMeasure                  */ EmptyFuncBV,
+        /* Log                          */ EmptyFuncBV,
+        /* FDrive_NumDirsAndFiles       */ Handler::GetNumDirsAndFiles,
+        /* FDrive_Mount                 */ Handler::IsMount,
+        /* FDrive_RequestDir            */ Handler::E,
+        /* FDrive_RequestFile           */ Handler::RequestFile,
+        /* Test                         */ EmptyFuncBV,
+        /* SetKoeffCalibration          */ EmptyFuncBV,
+        /* GetKoeffCalibration          */ EmptyFuncBV,
+        /* FDrive_RequestFileSize       */ Handler::RequestFileSize,
+        /* FDrive_RequestFileString     */ Handler::E,
+        /* FDrive_LoadFromExtStorage    */ Handler::LoadFromExtStorage,
+        /* FDrive_GetPictureDDS         */ Handler::E,
+        /* SCPI_RecvData                */ EmptyFuncBV
+    };
 
+    return funcs[com];
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+bool FDrive::Handler::IsMount()
+{
+    mounted = (Mount)msg->TakeByte();
+    Init();
+    return true;
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+bool FDrive::Handler::GetNumDirsAndFiles()
+{
+    return Items::Handler::Processing(msg);
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+bool FDrive::Handler::E()
+{
     return false;
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+bool FDrive::Handler::RequestFile()
+{
+    return Items::Handler::Processing(msg);
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+bool FDrive::Handler::RequestFileSize()
+{
+    return Items::Handler::Processing(msg);
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+bool FDrive::Handler::LoadFromExtStorage()
+{
+    return true;
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
