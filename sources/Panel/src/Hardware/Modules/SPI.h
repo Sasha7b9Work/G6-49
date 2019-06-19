ff42ddfa1bb4a014e3706ac4439e9fabff8f7928
Sasha7b9 <Sasha7b9@tut.by>
2019-06-19 13:41:36 +0300

    Рефакторинг

--- sources/Panel/src/Hardware/Modules/SPI.h
+++ sources/Panel/src/Hardware/Modules/SPI.h
@@ -1,24 +1,21 @@
 #pragma once
 
 
-class SPI4_
+namespace SPI4_
 {
-friend class CPU;
-private:
-    static void Init();
-public:
+    void Init();
     /// Переслать массив данных
-    static bool Transmit(const void *buffer, uint size, uint timeout);
+    bool Transmit(const void *buffer, uint size, uint timeout);
 
-    static bool Transmit(uint value, uint timeout);
+    bool Transmit(uint value, uint timeout);
     /// Принять массив данных
-    static bool Receive(void *recv, uint size, uint timeout);
+    bool Receive(void *recv, uint size, uint timeout);
     /// Функция принимает size байт и сравнивает их с compared. Возвращает количество дефектных байт
-    static uint ReceiveAndCompare(const void *compared, uint size);
+    uint ReceiveAndCompare(const void *compared, uint size);
     /// Возвращает true, если прибор готов к обмену
-    static bool IsReady();
+    bool IsReady();
     /// Ждать пока освободится устройство
-    static void WaitFreedom();
+    void WaitFreedom();
     /// Ожидание перехода сигнала готовности из состояния "занят" в состояние "свободен"
-    static void WaitFalling();
+    void WaitFalling();
 };
