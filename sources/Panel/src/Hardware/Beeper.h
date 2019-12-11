d8d353937b07d6156fa2385129cc04f77f7c31ba
Sasha7b9 <Sasha7b9@tut.by>
2019-12-11 17:01:45 +0300

    Начало динамика

---
+++ sources/Panel/src/Hardware/Beeper.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Settings/Settings.h"
+
+
+struct TypeWave
+{
+    enum E
+    {
+        Sine,
+        Meandr,
+        Triangle
+    } value;
+};
+
+
+struct Beeper
+{
+    static void Init();
+    /// Звук нажатия на кнопку.
+    static void ButtonPress();
+    /// Функция вызовет звук отпускаемой кнопки только если перед этим проигрывался звук нажатия кнопки.
+    static void ButtonRelease();
+
+    static void GovernorChangedValue();
+    
+    static void RegulatorShiftRotate();
+
+    static void RegulatorSwitchRotate();
+
+    static void WarnBeepBad();
+
+    static void WarnBeepGood();
+    /// Эту функцию надо вызывать перед записью/стиранием ППЗУ. Звук конфликтует с ППЗУ.
+    static void WaitForCompletion();
+
+    static void Beep(KeyEvent::Action::E type);
+
+    static void Test();
+
+    static uint8 Volume();
+
+    struct Bell
+    {
+        static void On();
+
+        static void Off();
+    };
+};
