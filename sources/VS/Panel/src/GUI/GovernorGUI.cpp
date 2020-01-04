b9721942cafe9d2cd253f3200a00e27102c86f53
Sasha7b9 <Sasha7b9@tut.by>
2020-01-04 11:19:50 +0300

    GUI Более-менее реалистичное поведение курсора мыши над ручкой

--- sources/VS/Panel/src/GUI/GovernorGUI.cpp
+++ sources/VS/Panel/src/GUI/GovernorGUI.cpp
@@ -7,6 +7,10 @@ GovernorGUI::GovernorGUI(wxWindow *parent, const wxPoint &position) : wxPanel(pa
     SetSize({ radius * 2 + 1, radius * 2 + 1 });
     SetDoubleBuffered(true);
     Bind(wxEVT_PAINT, &GovernorGUI::OnPaint, this);
+    Bind(wxEVT_MOTION, &GovernorGUI::OnMouseMove, this);
+    Bind(wxEVT_LEFT_DOWN, &GovernorGUI::OnMouseLeftDown, this);
+    Bind(wxEVT_LEFT_UP, &GovernorGUI::OnMouseLeftUp, this);
+    Bind(wxEVT_LEAVE_WINDOW, &GovernorGUI::OnMouseLeave, this);
 }
 
 
@@ -18,4 +22,69 @@ void GovernorGUI::OnPaint(wxPaintEvent &)
     dc.SetBrush(brush);
    
     dc.DrawCircle(radius, radius, radius);
+}
+
+
+void GovernorGUI::OnMouseMove(wxMouseEvent &event)
+{
+    if(leftIsDown)
+    {
+        ::SetCursorPos(positionDown.x, positionDown.y);
+    }
+
+    if(leftIsDown || MouseOnGovernor(event))
+    {
+        SetMouseCursorHand();
+    }
+}
+
+
+void GovernorGUI::OnMouseLeftDown(wxMouseEvent &event)
+{
+    if(MouseOnGovernor(event))
+    {
+        leftIsDown = true;
+
+        ::GetCursorPos(&positionDown);
+
+        SetMouseCursorHand();
+    }
+}
+
+
+void GovernorGUI::OnMouseLeftUp(wxMouseEvent &)
+{
+    leftIsDown = false;
+    SetMouseCursorHand();
+}
+
+
+void GovernorGUI::OnMouseLeave(wxMouseEvent &)
+{
+    if(leftIsDown)
+    {
+        ::SetCursorPos(positionDown.x, positionDown.y);
+
+        SetMouseCursorHand();
+    }
+}
+
+
+void GovernorGUI::SetMouseCursorHand()
+{
+    HCURSOR cursor = LoadCursor(NULL, IDC_HAND);
+
+    ::SetCursor(cursor);
+    ::ShowCursor(true);
+}
+
+
+bool GovernorGUI::MouseOnGovernor(wxMouseEvent &event)
+{
+    int mouseX = 0;
+    int mouseY = 0;
+
+    event.GetPosition(&mouseX, &mouseY);
+
+    return radius * radius >= (mouseX - radius) * (mouseX - radius) + (mouseY - radius) * (mouseY - radius);
 }
--- sources/VS/Panel/src/GUI/GovernorGUI.h
+++ sources/VS/Panel/src/GUI/GovernorGUI.h
@@ -9,6 +9,19 @@ class GovernorGUI : public wxPanel
 public:
     GovernorGUI(wxWindow *parent, const wxPoint &position);
 private:
-    void OnPaint(wxPaintEvent &);
     const int radius = 20;
+
+    bool leftIsDown = false;
+
+    POINT positionDown;
+
+    void OnPaint(wxPaintEvent &);
+    void OnMouseMove(wxMouseEvent &);
+    void OnMouseLeftDown(wxMouseEvent &);
+    void OnMouseLeftUp(wxMouseEvent &);
+    void OnMouseLeave(wxMouseEvent &);
+    /// ”становить курсор в виде руки
+    void SetMouseCursorHand();
+    /// ¬озвращает true, если курсор мыши находитс€ над изображением ручки
+    bool MouseOnGovernor(wxMouseEvent &);
 };
