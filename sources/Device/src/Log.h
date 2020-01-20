82a5fb4a9e30eb116830188f6bb30e200ba11fff
Sasha7b9 <Sasha7b9@tut.by>
2020-01-20 14:13:42 +0300

    *

--- sources/Device/src/Log.h
+++ sources/Device/src/Log.h
@@ -3,9 +3,9 @@
 
 
 
-#define LOG_WRITE(...)      Log::AddString(__VA_ARGS__)
-#define LOG_ERROR(...)      Log::AddString(__VA_ARGS__)
-#define LOG_FUNC_ENTER()    Log::AddString("%s %d enter", __FUNCTION__, __LINE__)
+#define LOG_WRITE(...)      ::Log::AddString(__VA_ARGS__)
+#define LOG_ERROR(...)      ::Log::AddString(__VA_ARGS__)
+#define LOG_FUNC_ENTER()    ::Log::AddString("%s %d enter", __FUNCTION__, __LINE__)
 #define LOG_ERROR_FINALIZE(...)
 
 
--- sources/Panel/src/Log.h
+++ sources/Panel/src/Log.h
@@ -1,12 +1,12 @@
 #pragma once   
 
 #if defined(DEBUG) && !defined(WIN32)
-#define LOG_WRITE(...)          Log::Write(TypeTrace::Info, __VA_ARGS__)
-#define LOG_WRITE_FINALIZE(...) Log::Write(TypeTrace::Info, __VA_ARGS__); Log::Finalize()
-#define LOG_ERROR(...)          Log::Write(TypeTrace::Error, __VA_ARGS__)
-#define LOG_ERROR_FINALIZE(...) Log::Write(TypeTrace::Error, __VA_ARGS__); Log::Finalize()
-#define LOG_ERROR_TRACE(...)    Log::Trace(TypeTrace::Error, __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
-#define LOG_FUNC_ENTER()        Log::Write(TypeTrace::Info, "%s enter", __FUNCTION__);
+#define LOG_WRITE(...)          ::Log::Write(TypeTrace::Info, __VA_ARGS__)
+#define LOG_WRITE_FINALIZE(...) ::Log::Write(TypeTrace::Info, __VA_ARGS__); Log::Finalize()
+#define LOG_ERROR(...)          ::Log::Write(TypeTrace::Error, __VA_ARGS__)
+#define LOG_ERROR_FINALIZE(...) ::Log::Write(TypeTrace::Error, __VA_ARGS__); Log::Finalize()
+#define LOG_ERROR_TRACE(...)    ::Log::Trace(TypeTrace::Error, __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define LOG_FUNC_ENTER()        ::Log::Write(TypeTrace::Info, "%s enter", __FUNCTION__);
 
 //#define LOG_WRITE_TRACE(...)    Log::Trace(TypeTrace::Info, __MODULE__, __FUNCTION__, __LINE__, __VA_ARGS__)
 // Когда нужен вспомогательный буфер для преобразования числа в строку, можно пользоваться этой функцией
@@ -18,9 +18,9 @@
 #elif defined(WIN32)
 #define ASSERT_RET(cont, ...)
 #define LOG_ERROR_TRACE(...)
-#define LOG_WRITE(...)          Log::Write(TypeTrace::Info, __VA_ARGS__)
+#define LOG_WRITE(...)          ::Log::Write(TypeTrace::Info, __VA_ARGS__)
 #define LOG_TRACE
-#define LOG_ERROR(...)          Log::Write(TypeTrace::Error, __VA_ARGS__)
+#define LOG_ERROR(...)          ::Log::Write(TypeTrace::Error, __VA_ARGS__)
 #define LOG_ERROR_FINALIZE(...)
 #define LOG_FUNC_ENTER()
 #define LOG_WRITE_FINALIZE(...)
