903d4890d756c8975568f203cdeb8302a4899277
Sasha7b9 <Sasha7b9@tut.by>
2020-02-05 11:01:53 +0300

    refact

--- sources/Panel/src/main.cpp
+++ sources/Panel/src/main.cpp
@@ -1,3 +1,4 @@
+#include "defines.h"
 #include "Display/Display.h"
 #include "Hardware/CPU.h"
 #include "Hardware/Timer.h"
@@ -11,17 +12,11 @@
 int main()
 {
     CPU::Init();
-    
     Timer::Init();
-    
     Display::Init();
-
     HAL_TIM::Delay(1000);    // Задержка введена, потому что без неё не запускается генератор. Видимо, он инициализируется гораздо быстрее панели
-
     setCal.Load();
-
     set.Load();
-
     Menu::Init();
 
     while (1)
