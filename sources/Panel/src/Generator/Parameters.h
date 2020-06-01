5d5040a6bcbafe9abcfa268509e6c84e66a0b306
Sasha7b9 <Sasha7b9@tut.by>
2020-06-02 00:19:07 +0300

    *

--- sources/Panel/src/Generator/Parameters.h
+++ sources/Panel/src/Generator/Parameters.h
@@ -238,7 +238,7 @@ public:
 class ParameterAmplitude : public ParameterVoltage
 {
 public:
-    ParameterAmplitude(const Value &min = Value("0"), const Value &max = Value("10"), const Value &value = Value("0")) :
+    ParameterAmplitude(const Value &min = Value("0"), const Value &max = Value("10"), const Value &value = Value("10")) :
         ParameterVoltage(ParameterDoubleType::Amplitude, "Размах", "Amplitude", min, max, value) { }
 };
 
