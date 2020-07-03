ed585191c863ecdcf76b35f2ab4f84910da3c66d
Sasha7b9 <Sasha7b9@tut.by>
2020-07-03 13:24:59 +0300

    Подправлена версия для сборки под Windows

--- sources/scripts/vs_win/build_release.bat
+++ sources/scripts/vs_win/build_release.bat
@@ -6,7 +6,7 @@ rmdir "generated/Win32" /s /q
 
 cd scripts/vs_win
 
-cmake ../../VS/CMakeLists.txt -B../../generated/Win32 -DCMAKE_SYSTEM_VERSION=10.0.17763.0 -G "Visual Studio 16 2019" -A Win32 -DCONF_RELEASE=ON
+cmake ../../VS/CMakeLists.txt -B../../generated/Win32 -DCMAKE_SYSTEM_VERSION=10.0.18362.0 -G "Visual Studio 16 2019" -A Win32 -DCONF_RELEASE=ON
 
 MSBuild.exe ..\..\generated\Win32\G6-49_GUI.sln -clp:ErrorsOnly;WarningsOnly -nologo /m
 set BUILD_STATUS=%ERRORLEVEL%
