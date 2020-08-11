e3f295561fadae35d4ad3de0f54190dc62339cfc
Sasha7b9 <Sasha7b9@tut.by>
2020-08-11 15:32:42 +0300

    Собирается и запускается из командной строки

--- sources/scripts/vs_win/copy_wxWidgets_dlls.bat
+++ sources/scripts/vs_win/copy_wxWidgets_dlls.bat
@@ -1,4 +1,4 @@
 @echo %TIME%   Copying files *.dll wxWidgtes
 
-xcopy ..\..\..\..\ThirdParty\wxWidgets\generated\lib\vc_dll\*.dll ..\..\generated\Win32\Panel\Debug /Y /Q
-xcopy ..\..\..\..\ThirdParty\wxWidgets\generated\lib\vc_dll\*.dll ..\..\generated\Win32\Editor\Debug /Y /Q
+xcopy ..\..\ThirdParty\wxWidgets\generated\lib\vc_dll\*.dll ..\..\generated\Win32\Panel\Debug /Y /Q
+xcopy ..\..\ThirdParty\wxWidgets\generated\lib\vc_dll\*.dll ..\..\generated\Win32\Editor\Debug /Y /Q
