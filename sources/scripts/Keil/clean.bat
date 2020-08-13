242e58e6d0bc7a0f77b67fb99451ecd9bc71b1b2
Sasha7b9 <Sasha7b9@tut.by>
2020-08-13 19:16:21 +0300

    *

--- sources/scripts/Keil/clean.bat
+++ sources/scripts/Keil/clean.bat
@@ -1,33 +1,18 @@
 @echo off
 
 if "%1" EQU "" goto HINT
-if %1==device  goto CLEAN_DEVICE
-if %1==panel   goto CLEAN_PANEL
-if %1==dloader goto CLEAN_DLOADER
-if %1==ploader goto CLEAN_PLOADER
+if %1==device  ( call :CLEAN Device  & goto EXIT )
+if %1==panel   ( call :CLEAN Panel   & goto EXIT )
+if %1==dloader ( call :CLEAN DLoader & goto EXIT )
+if %1==ploader ( call :CLEAN PLoader & goto EXIT )
 if %1==all     ( call clean.bat device & call clean.bat panel & call clean.bat dloader & call clean.bat ploader & goto EXIT )
 
 goto HINT
 
-:CLEAN_PANEL
-    echo Clean Panel
-    c:\Keil_v5\UV4\UV4 -c..\..\Panel\Panel.uvprojx -j0
-    goto EXIT
-
-:CLEAN_DEVICE
-    echo Clean Device
-    c:\Keil_v5\UV4\UV4 -c..\..\Device\Device.uvprojx -j0
-    goto EXIT
-
-:CLEAN_DLOADER
-    echo Clean Device Loader
-    c:\Keil_v5\UV4\UV4 -c..\..\DLoader\DLoader.uvprojx -j0
-    goto EXIT
-
-:CLEAN_PLOADER
-    echo Clean Panel Loader
-    c:\Keil_v5\UV4\UV4 -c..\..\PLoader\PLoader.uvprojx -j0
-    goto EXIT
+:CLEAN
+    echo Clean %1
+    c:\Keil_v5\UV4\UV4 -c..\..\%1\%1.uvprojx -j0
+    exit /b
 
 :HINT
 
--- sources/scripts/Keil/rebuild.bat
+++ sources/scripts/Keil/rebuild.bat
@@ -1,10 +1,12 @@
 @echo off
 
 if "%1" EQU "" goto HINT
-if %1==device  ( call clean.bat %1 & call build.bat %1 & goto EXIT )
-if %1==panel   ( call clean.bat %1 & call build.bat %1 & goto EXIT )
-if %1==dloader ( call clean.bat %1 & call build.bat %1 & goto EXIT )
-if %1==ploader ( call clean.bat %1 & call build.bat %1 & goto EXIT )
+set res=0
+if %1==device  set res=1
+if %1==panel   set res=1
+if %1==dloader set res=1
+if %1==ploader set res=1
+if %res%==1    ( call clean.bat %1 & call build.bat %1 & goto EXIT )
 if %1==all     ( call rebuild.bat device & call rebuild.bat panel & call rebuild.bat ploader & call rebuild.bat dloader & goto EXIT )
 
 goto HINT
