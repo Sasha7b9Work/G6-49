d5538753b7df96ec3e0a4b1f13c3d7cacda99ff7
Sasha7b9 <Sasha7b9@tut.by>
2020-08-13 15:32:53 +0300

    Скрипт для сборки device, panel, dloader, ploader

---
+++ sources/scripts/Keil/_build.bat
@@ -0,0 +1,12 @@
+@echo off
+
+@echo .
+@echo %TIME%   Build G6-49
+
+call build_device.bat
+
+call build_dloader.bat
+
+call build_panel.bat
+
+call build_ploader.bat
--- sources/scripts/Keil/build.bat
+++ sources/scripts/Keil/build.bat
@@ -1,12 +1,61 @@
 @echo off
 
-@echo .
-@echo %TIME%   Build G6-49
+if "%1" EQU "" goto HINT
+if %1==device goto BUILD_DEVICE
+if %1==panel goto BUILD_PANEL
+if %1==dloader goto BUILD_DLOADER
+if %1==ploader goto BUILD_PLOADER
 
-call build_device.bat
+goto EXIT
 
-call build_dloader.bat
+:BUILD_DEVICE
 
-call build_panel.bat
+    c:\Keil_v5\UV4\UV4 -b ..\..\Device\Device.uvprojx -j0 -z -o Device.out
+    set BUILD_STATUS=%ERRORLEVEL%
+    if %BUILD_STATUS%==0 goto BUILD_DEVICE_SUCCESS   
+    echo ERROR!!! Device    failed!!!
+    type ..\..\Device\Device.out
+    
+:BUILD_DEVICE_SUCCESS
+  
+    copy ..\..\Device\G6-49-D.bin h:\
+    goto EXIT
 
-call build_ploader.bat
+:BUILD_PANEL
+
+    c:\Keil_v5\UV4\UV4 -b ..\..\Panel\Panel.uvprojx -j0 -z -o Panel.out
+    set BUILD_STATUS=%ERRORLEVEL%
+    if %BUILD_STATUS%==0 goto BUILD_PANEL_SUCCESS
+    echo ERROR!!! Panel    failed!!!
+    type ..\..\Panel\Panel.out
+
+:BUILD_PANEL_SUCCESS
+    
+    copy ..\..\Panel\G6-49-P.bin h:\
+    goto EXIT
+
+:BUILD_DLOADER
+
+    c:\Keil_v5\UV4\UV4 -b ..\..\DLoader\DLoader.uvprojx -j0 -z -o DLoader.out
+    set BUILD_STATUS=%ERRORLEVEL%
+    if %BUILD_STATUS%==0 goto EXIT
+    echo ERROR!!! Device Loader   failed!!!
+    type ..\..\DLoader\DLoader.out
+    goto EXIT
+
+:BUILD_PLOADER
+
+    c:\Keil_v5\UV4\UV4 -b ..\..\PLoader\PLoader.uvprojx -j0 -z -o PLoader.out
+    set BUILD_STATUS=%ERRORLEVEL%
+    if %BUILD_STATUS%==0 goto EXIT
+    echo ERROR!!! Panel Loader   failed!!!
+    type ..\..\PLoader\PLoader.out
+    goto EXIT
+
+:HINT
+    echo.
+    echo Using:
+    echo       build.bat [device^|panel^|dloader^|ploader^|all]
+    goto EXIT
+
+:EXIT
