3410a800d9c19523bcd53df01fde5552f37a721b
Sasha7b9 <Sasha7b9@tut.by>
2020-08-13 18:12:48 +0300

    *

--- sources/scripts/Keil/build.bat
+++ sources/scripts/Keil/build.bat
@@ -1,61 +1,31 @@
 @echo off
 
 if "%1" EQU "" goto HINT
-if %1==device  goto BUILD_DEVICE
-if %1==panel   goto BUILD_PANEL
-if %1==dloader goto BUILD_DLOADER
-if %1==ploader goto BUILD_PLOADER
+if %1==device  ( call :BUILD "Compile Device" Device ..\..\Device\G6-49-D.bin & goto EXIT )
+if %1==panel   ( call :BUILD "Compile Panel" Panel ..\..\Panel\G6-49-P.bin & goto EXIT )
+if %1==dloader ( call :BUILD "Compile Device Loader" DLoader & goto EXIT )
+if %1==ploader ( call :BUILD "Compile Panel Loader" PLoader & goto EXIT )
 if %1==all     ( call build.bat device & call build.bat panel & call build.bat dloader & call build.bat ploader & goto EXIT)
 
 goto HINT
 
-:BUILD_DEVICE
+:BUILD
 
-    echo Compile Device
-    c:\Keil_v5\UV4\UV4 -b ..\..\Device\Device.uvprojx -j0 -z -o Device.out
+    echo %1
+    c:\Keil_v5\UV4\UV4 -b ..\..\%2\%2.uvprojx -j0 -z -o %2.out
     set BUILD_STATUS=%ERRORLEVEL%
-    if %BUILD_STATUS%==0 goto BUILD_DEVICE_SUCCESS
-    echo ERROR!!! Device    failed!!!
-    type ..\..\Device\Device.out
-    
-:BUILD_DEVICE_SUCCESS
+    if %BUILD_STATUS%==0 goto BUILD_SUCCESS
+    echo ERROR!!! %2    failed!!!
+    type ..\..\%2\%2.out
+    exit /b
+
+:BUILD_SUCCESS
   
-    copy ..\..\Device\G6-49-D.bin h:\
-    goto EXIT
-
-:BUILD_PANEL
-
-    echo Compile Panel
-    c:\Keil_v5\UV4\UV4 -b ..\..\Panel\Panel.uvprojx -j0 -z -o Panel.out
-    set BUILD_STATUS=%ERRORLEVEL%
-    if %BUILD_STATUS%==0 goto BUILD_PANEL_SUCCESS
-    echo ERROR!!! Panel    failed!!!
-    type ..\..\Panel\Panel.out
-
-:BUILD_PANEL_SUCCESS
-    
-    copy ..\..\Panel\G6-49-P.bin h:\
-    goto EXIT
-
-:BUILD_DLOADER
-
-    echo Compile Device Loader
-    c:\Keil_v5\UV4\UV4 -b ..\..\DLoader\DLoader.uvprojx -j0 -z -o DLoader.out
-    set BUILD_STATUS=%ERRORLEVEL%
-    if %BUILD_STATUS%==0 goto BUILD_PLOADER
-    echo ERROR!!! Device Loader   failed!!!
-    type ..\..\DLoader\DLoader.out
-    goto EXIT
-
-:BUILD_PLOADER
-
-    echo Compile Panel Loader
-    c:\Keil_v5\UV4\UV4 -b ..\..\PLoader\PLoader.uvprojx -j0 -z -o PLoader.out
-    set BUILD_STATUS=%ERRORLEVEL%
-    if %BUILD_STATUS%==0 goto EXIT
-    echo ERROR!!! Panel Loader   failed!!!
-    type ..\..\PLoader\PLoader.out
-    goto EXIT
+    if "%3" EQU "" exit /b
+    echo on
+    copy %3 h:\
+    echo off
+    exit /b
 
 :HINT
     echo.
