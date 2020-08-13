97c63613de497f60aab12b3a2b059137da1e5cbc
Sasha7b9 <Sasha7b9@tut.by>
2020-08-13 18:41:51 +0300

    *

--- sources/scripts/Keil/load.bat
+++ sources/scripts/Keil/load.bat
@@ -1,40 +1,20 @@
 @echo off
 
 if "%1" EQU "" goto HINT
-if %1==device  goto LOAD_DEVICE
-if %1==panel   goto LOAD_PANEL
-if %1==dloader goto LOAD_DLOADER
-if %1==ploader goto LOAD_PLOADER
-if %1==loaders ( call load.bat ploader & call load.bat dloader )
+if %1==device  ( call :LOAD Device  & goto EXIT )
+if %1==panel   ( call :LOAD Panel   & goto EXIT )
+if %1==dloader ( call :LOAD DLoader & goto EXIT )
+if %1==ploader ( call :LOAD PLoader & goto EXIT )
+if %1==loaders ( call load.bat ploader & call load.bat dloader & goto EXIT )
 if %1==all     ( call load.bat device & call load.bat panel & call load.bat ploader & call load.bat dloader & goto EXIT )
 
 goto HINT
 
-:LOAD_DEVICE
+:LOAD
+    echo Load firmware to %1
+    c:\Keil_v5\UV4\UV4 -f ..\..\%1\%1.uvprojx -j0 -o ..\scripts\Keil\out.txt
+    exit /b
 
-    echo Load firmware to Device
-    c:\Keil_v5\UV4\UV4 -f ..\..\Device\Device.uvprojx -j0 -o ..\scripts\Keil\out.txt
-    goto EXIT
-
-:LOAD_PANEL
-
-
-    echo Load firmware to Panel
-    c:\Keil_v5\UV4\UV4 -f ..\..\Panel\Panel.uvprojx -j0 -o ..\scripts\Keil\out.txt
-    goto EXIT
-
-:LOAD_PLOADER
-
-    echo Load firmware to Panel Loader
-    c:\Keil_v5\UV4\UV4 -f ..\..\PLoader\PLoader.uvprojx -j0 -o ..\scripts\Keil\out.txt
-    goto EXIT
-    
-:LOAD_DLOADER
-
-    echo Load firmware to Device Loader
-    c:\Keil_v5\UV4\UV4 -f ..\..\DLoader\DLoader.uvprojx -j0 -o ..\scripts\Keil\out.txt
-    goto EXIT
-    
 :HINT
 
     echo.
