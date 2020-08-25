02c83949e23a7acde981064f4c0efb17b5251fdd
Sasha7b9 <Sasha7b9@tut.by>
2020-08-25 11:22:44 +0300

    Универсальный скрипт make.bat для release- и debug- версии

--- sources/scripts/vs_win/make.bat
+++ sources/scripts/vs_win/make.bat
@@ -1,49 +1,50 @@
 @echo off
 
-if "%2" EQU "" goto LABEL1
-if %2==edit goto BUILD_EDIT
+if "%3" equ "" goto VERIFY_PARAMETER_1
+if %3==edit goto VERIFY_PARAMETER_1
 goto HINT
 
-:LABEL1
-    if "%1" EQU "" goto MAKE
-    if %1==build goto MAKE
+:VERIFY_PARAMETER_1
+    if "%1" equ "" goto HINT
+    if "%2" equ "" goto VERIFY_PARAMETER_MAKE
+    if %1==build goto VERIFY_PARAMETER_MAKE_BUILD
+    goto HINT
+
+:VERIFY_PARAMETER_MAKE
+    if %1==debug ( call :MAKE debug & goto EXIT )
+    if %1==release ( call :MAKE release & goto EXIT )
+    goto HINT
+
+:VERIFY_PARAMETER_MAKE_BUILD
+    if "%1" equ "" goto HINT
+    if %2==debug (call :MAKE %2 & call build.bat & goto VERIFY_PARAMETER_EDIT )
+    if %2==release (call :MAKE %2 & call build.bat & goto VERIFY_PARAMETER_EDIT )
+    goto HINT
+
+:VERIFY_PARAMETER_EDIT
+    if "%3" equ "" goto EXIT
+    if %3==edit ( call edit.bat & goto EXIT )
     goto HINT
 
 :MAKE
     @echo.
     @echo %TIME%   Making Visual Studio Win Debug ...
-    
     cd ../..
-    
     rmdir "generated/Win32" /s /q
-    
     cd scripts/vs_win
-    
-    @echo on
-    cmake ../../VS/CMakeLists.txt -B../../generated/Win32 -G "Visual Studio 16 2019" -A Win32
-    
+    if %1==debug ( @echo on & cmake ../../VS/CMakeLists.txt -B../../generated/Win32 -G "Visual Studio 16 2019" -A Win32 )
     @echo off
-    
+    if %1==release ( @echo on & cmake ../../VS/CMakeLists.txt -B../../generated/Win32 -G "Visual Studio 16 2019" -A Win32 -DBUILD_USE_STATIC_RUNTIME=ON )
+    @echo off
     @echo %TIME%   Complete
-    
-    if "%1" EQU "" goto EXIT
-    if %1==build goto BUILD
-    goto HINT
-    
-
-:BUILD
-    call build.bat
-    goto EXIT
+    exit /b
 
 :HINT
     echo.
     echo Using:
-    echo       make [build [edit]]
+    echo       make.bat ^<debug^|release^>
+    echo       make.bat build ^<debug^|release^> [edit]
     echo.
     goto EXIT
-
-:BUILD_EDIT
-    if %1==build ( call make.bat build & call edit.bat & goto EXIT )
-    goto HINT
 
 :EXIT
