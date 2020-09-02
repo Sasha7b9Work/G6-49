8d08e9fd95fa9a76cf07de2eeee2e705e8b28244
Sasha7b9 <Sasha7b9@tut.by>
2020-09-02 12:19:39 +0300

    Исправлена генерация sin(x)/x в редакторе

--- sources/VS/Editor/src/Dialogs/SinXDialog.cpp
+++ sources/VS/Editor/src/Dialogs/SinXDialog.cpp
@@ -31,13 +31,13 @@ SinXDialog::SinXDialog() : Dialog(wxT("Параметры сигнала sin(x)/x"), true)
 
 void SinXDialog::SendAdditionForm()
 {
-    float T = static_cast<float>(Point::NUM_POINTS / 2) / static_cast<float>(scNumPeriods->GetValue());
+    float T = static_cast<float>(Point::NUM_POINTS / 2) / (static_cast<float>(scNumPeriods->GetValue()) + 0.25F);
 
     int i0 = Point::NUM_POINTS / 2;
 
     for (int i = 0; i < Point::NUM_POINTS / 2; i++)
     {
-        float x = static_cast<float>(i) / T * 2.0F * 3.14F;
+        float x = static_cast<float>(i) / T * 2.0F * static_cast<float>(M_PI);
 
         float sinx_x = std::sinf(x) / x;
 
