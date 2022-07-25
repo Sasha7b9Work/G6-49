8433fdf4fc66e60dee5583839436fa3634efd564
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-07-25 21:01:07 +0300

    *

--- sources/Panel/src/Generator/MathSupport.h
+++ sources/Panel/src/Generator/MathSupport.h
@@ -8,23 +8,23 @@ class ParameterDouble;
 class ParameterInteger;
 
 
-struct MathDouble
+namespace MathDouble
 {
     // ¬озвращает числовое представление Value с numDigits значащими цифрами и зап€той.
-    static pString GetIndicatedValue(const ParameterDouble *param);
+    pString GetIndicatedValue(const ParameterDouble *param);
 
     // ¬озвращает позицию первого ненулевого символа "1" - дес€тки (1e1), "0" - единицы (1e0), "-1" - дес€тые (1e-1), "-2" - сотые (1e-2)
-    static int GetPositionFirstDigit(const ParameterDouble *param, Order::E order = Order::Count);
+    int GetPositionFirstDigit(const ParameterDouble *param, Order::E order = Order::Count);
 
-    static int GetPositionFirstDigit(const Value &value, Order::E order);
+    int GetPositionFirstDigit(const Value &value, Order::E order);
 
     // ¬озвращает цифру в позиции position. “очка находитс€ соответственно order. One - после единиц, Kilo - после тыс€ч и так далее.
     // Order::Count - значенине по умолчанию - зап€та€ в позиции относительно размерности числового значени€
-    static int GetDigit(const Value &value, int position, Order::E order = Order::Count);
+    int GetDigit(const Value &value, int position, Order::E order = Order::Count);
 
     // ¬озвращает символ в позиции position. «нак не учитываетс€. “очка находитс€ соответственно order. One - после единиц, Kilo - после тыс€ч и так далее.
     // Order::Count - значенине по умолчанию - зап€та€ в позиции относительно размерности числового значени€
-    static char GetChar(const Value &value, int postition, Order::E order);
+    char GetChar(const Value &value, int postition, Order::E order);
 };
 
 
@@ -45,8 +45,8 @@ private:
 };
 
 
-struct MathParameterInteger
+namespace MathParameterInteger
 {
     // ¬озвращает количество разр€дов дл€ параметра parameter (в них должно помещатьс€ максимально возможное значение параметра parameter)
-    static int GetMaxNumberDigits(const ParameterInteger *parameter);
+    int GetMaxNumberDigits(const ParameterInteger *parameter);
 };
