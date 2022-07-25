f8abe2e55a6efe9ad3f04bee6e40cb7e18db552c
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-07-25 21:10:51 +0300

    *

--- sources/Panel/src/Keyboard/Keyboard.h
+++ sources/Panel/src/Keyboard/Keyboard.h
@@ -3,24 +3,24 @@
 #include "Keyboard/Controls.h"
 
 
-struct Keyboard
+namespace Keyboard
 {
-    friend class CPU;
+    void Init();
 
-public:
-    
-    static void InitInputs(const uint16 *sl, const char *portSL, int numSL, const uint16 *rl, const char *portRL, int numRL);
+    void Update();
+
+    void InitInputs(const uint16 *sl, const char *portSL, int numSL, const uint16 *rl, const char *portRL, int numRL);
     
     // Возвращает true, если буфер пуст
-    static bool BufferIsEmpty();
+    bool BufferIsEmpty();
     
     // Возвращает следующий орган управления, если таковой имеется
-    static Control GetNextControl();
+    Control GetNextControl();
     
     // Функция-заглушка. Для эмулятора, на котором нет физической клавиатуры и нужно её рисовать на экране
-    static void Draw();
+    void Draw();
 
-    static void AppendEvent(Key::E key, Action::E action);
+    void AppendEvent(Key::E key, Action::E action);
 
     // Структура предназначена единственно для декодирования клавиатурных последовательностей
     struct Decoder
@@ -30,10 +30,4 @@ public:
         // Размер массива не может превышать 20
         static bool Decode(const Control controls[20], const Control &control);
     };
-
-private:
-
-    static void Init();
-
-    static void Update();
 };
