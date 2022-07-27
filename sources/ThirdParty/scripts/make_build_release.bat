b82168d5e3b258884b08164ef480a281ca98fedf
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-07-27 22:02:48 +0300

    *

--- sources/ThirdParty/scripts/make_build_release.bat
+++ sources/ThirdParty/scripts/make_build_release.bat
@@ -1,5 +1,5 @@
 cd ../wxWidgets
 rmdir generated /s /q
 cd ../scripts
-cmake.exe ..\wxWidgets\CMakeLists.txt -B..\wxWidgets\generated -G "Visual Studio 16 2019" -A Win32 -DwxBUILD_SHARED=ON -DwxBUILD_USE_STATIC_RUNTIME=ON
+cmake.exe ..\wxWidgets\CMakeLists.txt -B..\wxWidgets\generated -G "Visual Studio 17 2022" -A Win32 -DwxBUILD_SHARED=ON -DwxBUILD_USE_STATIC_RUNTIME=ON
 call build.bat
