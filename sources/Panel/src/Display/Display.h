698d02231333e9c0ee30dc9552996d7148387398
Sasha7b9Work <Phuntik-it@tut.by>
2022-07-27 15:03:10 +0300

    refact

--- sources/Panel/src/Display/Display.h
+++ sources/Panel/src/Display/Display.h
@@ -8,29 +8,29 @@
 #define BUFFER_HEIGHT   Display::HEIGHT   // Высота буфера для рисования
 
 
-struct Display
+namespace Display
 {
     static const int WIDTH = 320;
     static const int HEIGHT = 240;
 
-    static void Init();
+    void Init();
 
-    static void Update();
+    void Update();
 
-    static void DrawScreenSplash();
+    void DrawScreenSplash();
 
-    static uint8 *GetBuffer();
+    uint8 *GetBuffer();
 
-    static uint8 *GetRow(int row);
+    uint8 *GetRow(int row);
     
-    static void ShowWarning(String warning);
-    static void ShowWarning(const char *warning);
+    void ShowWarning(String warning);
+    void ShowWarning(const char *warning);
 
-    static void ClearWarnings();
+    void ClearWarnings();
 
     // Экран
-    static uint8 frontBuffer[BUFFER_WIDTH * BUFFER_HEIGHT];
+    extern uint8 frontBuffer[BUFFER_WIDTH * BUFFER_HEIGHT];
 
     // Задний буфер. В нём происходит отрисовка, и затем изображение копируется во frontBuffer
-    static uint8 backBuffer[BUFFER_WIDTH * BUFFER_HEIGHT];
+    extern uint8 backBuffer[BUFFER_WIDTH * BUFFER_HEIGHT];
 };
