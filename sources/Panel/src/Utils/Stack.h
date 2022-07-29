dba33052fa098c89c9cf874d19cde7f8ee40c3df
Sasha7b9Work <Phuntik-it@tut.by>
2022-07-29 16:04:46 +0300

    *

--- sources/Panel/src/Utils/Stack.h
+++ sources/Panel/src/Utils/Stack.h
@@ -7,17 +7,17 @@ class Stack
 {
 public:
     Stack(int size);
-    
+
     ~Stack();
-    
+
     void Push(T elem);
-    
+
     T Pop();
-    
+
     int Size() const;
-    
+
     bool IsEmpty() const;
-    
+
     T &operator[](const int index) const;
 
     void Clear();
--- sources/Panel/src/Utils/StringUtils.h
+++ sources/Panel/src/Utils/StringUtils.h
@@ -4,7 +4,7 @@
 
 struct Word
 {
-    char   *address;
+    char *address;
     int8    numSymbols;
     uint8   notUsed0;
     uint8   notUsed1;
@@ -12,10 +12,10 @@ struct Word
 };
 
 
-struct Locale
+namespace Locale
 {
-    static void FindSeparator();
-    static uint8 separator;
+    void FindSeparator();
+    extern uint8 separator;
 };
 
 
@@ -25,7 +25,7 @@ struct SU
     static char *FindEnd(char *buffer);
 
     static char *Float2String(float value);
-    
+
     // \brief Переводит число с плавающей точкой в текстовый вид. numDigits - число цифровых знакомест
     // \attention Строка будет храниться до следующего вызова функции. Если результат нужен большее количество времени, то его нужно скопироавать себе.
     // \retval указатель на строку с числом.
@@ -36,7 +36,7 @@ struct SU
     static bool String2Int(pchar buffer, int *value, char **end);
 
     static String Int2String(int value, bool alwaysSign, int numMinFields);
-    
+
     // @brief Преобразует value в текстовую строку
     // @attention Строка будет храниться до следующего вызова функции. Если результат нужен большее количество времени, то его нужно скопировать себе
     // \param numMinFields минимальное число занимаемых знакомест. Если для вывода числа столько не требуется, лишние заполняются нулями
@@ -45,30 +45,30 @@ struct SU
     static char *UInt64_2String(uint64 value, char bufferOut[20] = 0);
 
     static char *UInt2String(uint value,                // значение
-                             char bufferOut[20] = 0     // сюда записывается возвращаемое значение
+        char bufferOut[20] = 0     // сюда записывается возвращаемое значение
     );
-    
+
     // Перевести десятичное число в символьную строку длиной allDigits. После точка отстоит на forFract символов от конца
     static char *UInt2StringThisPoint(uint value, char bufferOut[20], int allDigits, int forFract);
-    
+
     // Преобразует двоичное число ,представленное строкой из нулей и единиц, в uint
     static uint StringToBin32(char buffer[33]);
 
     static char *Buffer2FloatString(const uint8 *buffer);
-    
+
     // Возвращает число слов в строке string
     static int NumWords(pchar string);
-    
+
     // Возвращает указатель на n слово в строке. Если char == 0 - слова нет, если ret value == 0xffffffff - выходной буфер слишком мал
     static char *GetWord(char *string, int n, char *out, int size);
-    
+
     // Вычисляет число разрядов в целом типа int.
     static int NumDigitsInNumber(int value);
-    
+
     // Эта команда сразу преобразует к верхенму регистру слово.
     static bool GetWord(pchar string, Word *word, const int numWord);
 
-    static bool WordEqualZeroString(Word *word, char* string);
+    static bool WordEqualZeroString(Word *word, char *string);
 
     static int FindSymbol(pchar string, char symbol);
 
@@ -90,10 +90,10 @@ struct SU
     static char *Bin2StringN(uint value, char buffer[33], int n);
 
 private:
-    
+
     // Возвращает false, если выбор невозможен - строка кончилась.
     static bool ChooseSymbols(pchar *string);
-    
+
     // Возвращает false, если выбор невозможен - строка кончилась.
     static bool ChooseSpaces(pchar *string);
 };
