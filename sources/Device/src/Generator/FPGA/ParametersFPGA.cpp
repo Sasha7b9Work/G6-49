c15ece357392f4c0e267981ba00d66709f5fcecf
Sasha7b9Work <Phuntik-it@tut.by>
2022-09-09 09:25:15 +0300

    *

---
+++ sources/Device/src/Generator/FPGA/ParametersFPGA.cpp
@@ -0,0 +1,3 @@
+// 2022/09/09 09:22:14 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Generator/FPGA/ParametersFPGA.h"
---
+++ sources/Device/src/Generator/FPGA/ParametersFPGA.h
@@ -0,0 +1,19 @@
+// 2022/09/09 09:22:26 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+#include "Common/Common.h"
+
+
+namespace FPGA
+{
+    namespace Parameter
+    {
+        namespace A
+        {
+            namespace PeriodPacket
+            {
+                void Set(const Value &);
+                const Value &Get();
+            }
+        }
+    }
+}
