a52e9899219d57a4748b60a79ea92ec6c68f046b
Sasha7b9Work <Phuntik-it@tut.by>
2022-10-18 14:48:49 +0300

    *

---
+++ sources/Device/src/Generator/Filtr.h
@@ -0,0 +1,11 @@
+// 2022/10/18 14:45:05 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+#include "Common/Common.h"
+
+
+namespace Filtr
+{
+    void Init();
+
+    void Tune(const Chan &ch, TypeForm::E form);
+}
--- sources/Device/src/Generator/Generator_d.cpp
+++ sources/Device/src/Generator/Generator_d.cpp
@@ -3,6 +3,7 @@
 #include "Log.h"
 #include "Generator/Generator_d.h"
 #include "FreqMeter/FreqMeter_d.h"
+#include "Generator/Filtr.h"
 #include <cmath>
 
 
@@ -28,80 +29,6 @@ bool Amplifier::IsEnabled(const Chan &ch)
 {
     return isEnabled[ch];
 }
-
-
-struct Filtr
-{
-    static void Init()
-    {
-        HAL_PIO::Init(WR_P1_AMPL_A, HMode::Output_PP, HPull::No); //-V525
-        HAL_PIO::Init(WR_P2_AMPL_B, HMode::Output_PP, HPull::No);
-        HAL_PIO::Init(HPort::_E, HPin::_14, HMode::Output_PP, HPull::No);
-
-        HAL_PIO::Init(HPort::_F, HPin::_4, HMode::Output_PP, HPull::No);
-    }
-
-    static void Tune(const Chan &ch, TypeForm::E form)
-    {
-        if (form == TypeForm::Sine)
-        {
-            SetType(ch, Type::Chebyshev);
-        }
-        else if(form == TypeForm::Impulse || form == TypeForm::Packet || form == TypeForm::Meander)
-        {
-            SetType(ch, Type::None);
-        }
-        else
-        {
-            SetType(ch, Type::Bessel);
-        }
-    }
-
-private:
-
-    struct Type
-    {
-        enum E
-        {
-            None,
-            Chebyshev,
-            Bessel,
-            Count
-        };
-    };
-
-    static void SetType(const Chan &ch, Type::E type)
-    {
-        /*  +-----------+---------+---------+
-            |           | Канал А | Канал B |
-            +-----------+----+----+----+----+
-            |           |PE12|PE14|PE13|PF4 |
-            |  Сигнал   | p1 | p3 | p2 | p8 |
-            +-----------+----+----+----+----+
-            | Синус     | X    1  | X    1  |
-            +-----------+---------+---------+
-            | Импульс   |         |         |
-            | Пакет     | 0    0  | 0    0  |
-            | Меандр    |         |         |
-            +-----------+---------+---------+
-            | остальные | 1    0  | 1    0  |
-            +-----------+---------+---------+
-        */
-
-        static const HPort::E gpio0[Chan::Count] = { HPort::_E, HPort::_E };
-        static const HPort::E gpio1[Chan::Count] = { HPort::_E, HPort::_F };
-
-        static const uint16 pin0[Chan::Count] = { HPin::_12, HPin::_13 };
-        static const uint16 pin1[Chan::Count] = { HPin::_14, HPin::_4 };
-        
-        //                                                   нет         Чебышев      Бессель
-        static const HState::E state0[Type::Count] = { HState::Reset, HState::Set, HState::Set   };
-        static const HState::E state1[Type::Count] = { HState::Reset, HState::Set, HState::Reset };
-
-        HAL_PIO::Write(gpio0[ch], pin0[ch], state0[type]);
-        HAL_PIO::Write(gpio1[ch], pin1[ch], state1[type]);
-    }
-};
 
 
 void DGenerator::Init()
